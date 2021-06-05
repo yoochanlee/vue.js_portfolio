2eb370764f43b678d5e042c2e25623ca7f418c06
yoochan <leee302@naver.com>
2021-06-05 13:41:27 +0900

    dd

---
+++ project/front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <top></top>
+    <div id="container" class="container">
+        <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import top from './components/header/header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    top
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ project/front/src/components/page/login.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h3>login form</h3>
+  <div>
+    <label for='id'>ID</label>
+    <input type="text" id='id' v-model='id' />
+  </div>
+  <div>
+    <label for='pwd'>PWD</label>
+    <input type="password" id='pwd' v-model='pwd' />
+  </div>
+  <button v-on:click='login'>login</button>
+  <router-link to='/join'>join</router-link>
+</div>
+</template>
+
+
